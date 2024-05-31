7a730b95606bf6d0d9e79d8375d9fad0aec46237
William Boone <138145899+boonewh@users.noreply.github.com>
2024-05-31 17:52:36 -0500

    Front page responsive, form page PC started

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from forms import ContactForm
+from flask_mail import Mail, Message
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -10,9 +16,29 @@ def index():
 def pricing():
     return render_template('pricing.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            try:
+                msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
+                msg.body = f""" 
+                This message was sent from the PathSix Web Design contact form.
+
+                From: {form.name.data} <{form.email.data}> 
+                {form.message.data} 
+                """
+                mail.send(msg)
+                return render_template('contact.html', success=True)
+            except Exception as e:
+                flash('An error occurred while sending the message. Please try again later.')
+                return render_template('contact.html', form=form)
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
