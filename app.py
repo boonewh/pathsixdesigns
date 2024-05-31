de22ea35a69e311a5b1df2ba2c831b4d3a9fd79c
William Boone <138145899+boonewh@users.noreply.github.com>
2024-05-31 12:44:37 -0500

    Working on home page

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
