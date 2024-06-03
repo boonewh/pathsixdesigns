3b7811c550c35c12311716432ce251f29705b59c
William Boone <138145899+boonewh@users.noreply.github.com>
2024-06-03 18:53:01 -0500

    Feedback updates

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import ContactForm
 from flask_mail import Mail, Message
-from config import Config
+from config import EMAIL_PASSWORD, EMAIL_ADDRESS
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.secret_key = 'pathsix033170'
 
-mail = Mail(app)
+mail = Mail()
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = EMAIL_ADDRESS
+app.config["MAIL_PASSWORD"] = EMAIL_PASSWORD
+mail.init_app(app)
 
 @app.route('/')
 def index():
@@ -18,26 +24,20 @@ def pricing():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    form = ContactForm()
-    if form.validate_on_submit():  # This replaces 'if request.method == 'POST':'
-        try:
-            msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
-            msg.body = f"""
-            This message was sent from the PathSix Web Design contact form.
-
-            From: {form.name.data} <{form.email.data}>
-            {form.message.data}
-            """
-            mail.send(msg)
-            return render_template('contact.html', success=True)
-        except Exception as e:
-            flash('An error occurred while sending the message. Please try again later.')
-            return render_template('contact.html', form=form)
+  form = ContactForm()
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
     else:
-        # This else block now captures both GET requests and failed POST validations.
-        if request.method == 'POST':
-            flash('All fields are required.')  # You might want to keep or remove this line depending on your preference
-        return render_template('contact.html', form=form)
+      msg = Message(form.subject.data, sender=EMAIL_ADDRESS, recipients=[EMAIL_ADDRESS])
+      msg.body = """ 
+        This message was sent from the PathSix Web Design contact form.
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        From: %s <%s> 
+        %s 
+        """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+      return render_template('contact.html', success=True)
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
