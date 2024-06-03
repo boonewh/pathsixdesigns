ea54b19ac6d1b100c8002533439848ccc2050ff8
William Boone <138145899+boonewh@users.noreply.github.com>
2024-06-02 21:54:43 -0500

    MVP completed

--- app.py
+++ app.py
@@ -19,25 +19,24 @@ def pricing():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
-    if request.method == 'POST':
-        if form.validate() == False:
-            flash('All fields are required.')
-            return render_template('contact.html', form=form)
-        else:
-            try:
-                msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
-                msg.body = f""" 
-                This message was sent from the PathSix Web Design contact form.
+    if form.validate_on_submit():  # This replaces 'if request.method == 'POST':'
+        try:
+            msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
+            msg.body = f"""
+            This message was sent from the PathSix Web Design contact form.
 
-                From: {form.name.data} <{form.email.data}> 
-                {form.message.data} 
-                """
-                mail.send(msg)
-                return render_template('contact.html', success=True)
-            except Exception as e:
-                flash('An error occurred while sending the message. Please try again later.')
-                return render_template('contact.html', form=form)
-    elif request.method == 'GET':
+            From: {form.name.data} <{form.email.data}>
+            {form.message.data}
+            """
+            mail.send(msg)
+            return render_template('contact.html', success=True)
+        except Exception as e:
+            flash('An error occurred while sending the message. Please try again later.')
+            return render_template('contact.html', form=form)
+    else:
+        # This else block now captures both GET requests and failed POST validations.
+        if request.method == 'POST':
+            flash('All fields are required.')  # You might want to keep or remove this line depending on your preference
         return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, Email
 class ContactForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(message="Please enter your name.")])
     email = StringField("Email", validators=[DataRequired(message="Please enter your email address"), Email()])
-    subject = StringField("Subject", validators=[DataRequired(message="Please enter a subject.")])
-    message = TextAreaField("Message", validators=[DataRequired(message="Please enter a message.")])
+    subject = StringField("What is your current website? (ex: www.name.com or \"none\" is also fine.)", validators=[DataRequired(message="Please enter a site or none.")])
+    message = TextAreaField("Briefly describe what you need in your new website. \"Not sure\" is a valid reponse.", validators=[DataRequired(message="Please enter a message. I don't know or I want to talk about it are good responses.")])
     submit = SubmitField("Send")
