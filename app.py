285c469e54cbae783930d72d4d7f0167ed65e88e
William Boone <138145899+boonewh@users.noreply.github.com>
2024-08-21 22:33:16 -0500

    rejecting certain messages

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import ContactForm
 from flask_mail import Mail, Message
-from config import EMAIL_PASSWORD, EMAIL_ADDRESS
+from config import Config
 
 app = Flask(__name__)
-app.secret_key = 'pathsix033170'
+app.config.from_object(Config)
 
-mail = Mail()
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = EMAIL_ADDRESS
-app.config["MAIL_PASSWORD"] = EMAIL_PASSWORD
-mail.init_app(app)
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -24,20 +18,23 @@ def pricing():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-  form = ContactForm()
-  if request.method == 'POST':
-    if form.validate() == False:
-      flash('All fields are required.')
-      return render_template('contact.html', form=form)
-    else:
-      msg = Message(form.subject.data, sender=EMAIL_ADDRESS, recipients=[EMAIL_ADDRESS])
-      msg.body = """ 
-        This message was sent from the PathSix Web Design contact form.
+    form = ContactForm()
+    if request.method == 'POST':
+        if not form.validate():
+            if "Your message has been rejected. Please Stop." in form.name.errors:
+                flash("Your message has been rejected.")
+            else:
+                flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'], recipients=[app.config['MAIL_USERNAME']])
+            msg.body = """ 
+            This message was sent from the PathSix Web Design contact form.
 
-        From: %s <%s> 
-        %s 
-        """ % (form.name.data, form.email.data, form.message.data)
-      mail.send(msg)
-      return render_template('contact.html', success=True)
-  elif request.method == 'GET':
-    return render_template('contact.html', form=form)
+            From: %s <%s> 
+            %s 
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            return render_template('contact.html', success=True)
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
--- forms.py
+++ forms.py
@@ -1,11 +1,15 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
 
-# Create a form class
+def validate_name(form, field):
+    if "RobertHiene" in field.data:
+        raise ValidationError("Your message has been rejected. Please Stop.")
+
 class ContactForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired(message="Please enter your name.")])
+    name = StringField("Name", validators=[DataRequired(message="Please enter your name."), validate_name])
     email = StringField("Email", validators=[DataRequired(message="Please enter your email address"), Email()])
-    subject = StringField("What is your current website? (ex: www.name.com or \"none\" is also fine.)", validators=[DataRequired(message="Please enter a site or none.")])
-    message = TextAreaField("Briefly describe what you need in your new website. \"Not sure\" is a valid reponse.", validators=[DataRequired(message="Please enter a message. I don't know or I want to talk about it are good responses.")])
+    subject = StringField("What is your current website? (ex: www.name.com or 'none' is also fine.)", validators=[DataRequired(message="Please enter a site or none.")])
+    message = TextAreaField("Briefly describe what you need in your new website. 'Not sure' is a valid response.", validators=[DataRequired(message="Please enter a message. I don't know or I want to talk about it are good responses.")])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Send")
